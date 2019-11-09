72dfdfa9aeb5d9ff8426225c482bfda6c38dd496
Shuma Yoshioka <s64.stdio@gmail.com>
2019-11-10 00:48:01 +0900

    Add dummy responses

--- MyBot.cs
+++ MyBot.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 
@@ -7,13 +9,29 @@ namespace BotFrameworkExample
     public class MyBot : ActivityHandler
     {
 
+        private static readonly string[] DUMMY = new string[] { "blah-blah-blah", "something something" };
+
         protected override async Task OnMessageActivityAsync(
             ITurnContext<Microsoft.Bot.Schema.IMessageActivity> turnContext,
             System.Threading.CancellationToken cancellationToken
         )
         {
+            string response;
+
+            if (turnContext.Activity.Text.StartsWith("Hello")) {
+                response = "Hello!";
+            } else if (turnContext.Activity.Text.StartsWith("Hi")) {
+                response = "Hi!";
+            } else if (turnContext.Activity.Text.StartsWith("テスト")) {
+                response = "🙆";
+            } else {
+                response = DUMMY.OrderBy(_ => Guid.NewGuid()).First();
+            }
+
             await turnContext.SendActivityAsync(
-                MessageFactory.Text("Hello World!"),
+                MessageFactory.Text(
+                    response
+                ),
                 cancellationToken
             );
         }
